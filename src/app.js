b0ba9363eae3abcc1b6b942683d03e344fb08619
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2025-08-12 10:06:49 +0530

    added cors

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 import userRouter from "./routes/user.routes.js"
 import messageRouter from "./routes/message.routes.js"
